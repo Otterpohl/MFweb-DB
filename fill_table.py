baf2bb5a421ef03400b4ca31b994faf354517e7f
installer <installer@gmail.com>
2025-01-03 15:37:27 +0000

    change db to postgres and add python requirements

--- fill_table.py
+++ fill_table.py
@@ -6,10 +6,10 @@
 
 # Connect to the DB 
 import sqlalchemy as db
-import scipy.io
+import h5py
 
 # Establish the connection with the DB: 
-engine     = db.create_engine('mysql+pymysql://root:123456??@localhost/MF', echo = True)
+engine     = db.create_engine('postgresql://root@localhost:5432/mf', echo = True)
 metadata   = db.MetaData()
 connection = engine.connect()
 
@@ -30,60 +30,61 @@ for task_no in range(1,101):
     #Inserting many records at ones
     query  = db.insert(table) 
 
-    mat = scipy.io.loadmat(folder_usr + 'Task.mat')
-    tab = mat.get("Task")
-    for i in range(0,len(tab)):
-         Tab = dict()
-         Tab['TaskNo']              = tab[i,0].tolist()
-         Tab['TrialNo']             = tab[i,1].tolist()
-         Tab['BlockNo']             = tab[i,2].tolist()
-         Tab['Horizon']             = tab[i,3].tolist()
-         Tab['ItemNo']              = tab[i,4].tolist()
-         Tab['InitialSampleNb']     = tab[i,5].tolist()
-         Tab['UnusedTree']          = tab[i,6].tolist()      
-         Tab['DisplayOrder1']       = tab[i,7].tolist()
-         Tab['DisplayOrder2']       = tab[i,8].tolist()
-         Tab['DisplayOrder3']       = tab[i,9].tolist()     
-         Tab['TreePositions1']      = tab[i,10].tolist()
-         Tab['TreePositions2']      = tab[i,11].tolist()
-         Tab['TreePositions3']      = tab[i,12].tolist()
-         Tab['TreePositions4']      = tab[i,13].tolist()       
-         Tab['InitialSample1Tree']  = tab[i,14].tolist()
-         Tab['InitialSample2Tree']  = tab[i,15].tolist()
-         Tab['InitialSample3Tree']  = tab[i,16].tolist()
-         Tab['InitialSample4Tree']  = tab[i,17].tolist()
-         Tab['InitialSample5Tree']  = tab[i,18].tolist()
-         Tab['InitialSample1Size']  = tab[i,19].tolist()
-         Tab['InitialSample2Size']  = tab[i,20].tolist()
-         Tab['InitialSample3Size']  = tab[i,21].tolist()
-         Tab['InitialSample4Size']  = tab[i,22].tolist()
-         Tab['InitialSample5Size']  = tab[i,23].tolist()
-         Tab['Tree1FutureSize1']    = tab[i,24].tolist()
-         Tab['Tree1FutureSize2']    = tab[i,25].tolist()
-         Tab['Tree1FutureSize3']    = tab[i,26].tolist()
-         Tab['Tree1FutureSize4']    = tab[i,27].tolist()
-         Tab['Tree1FutureSize5']    = tab[i,28].tolist()
-         Tab['Tree1FutureSize6']    = tab[i,29].tolist()
-         Tab['Tree2FutureSize1']    = tab[i,30].tolist()
-         Tab['Tree2FutureSize2']    = tab[i,31].tolist()
-         Tab['Tree2FutureSize3']    = tab[i,32].tolist()
-         Tab['Tree2FutureSize4']    = tab[i,33].tolist()
-         Tab['Tree2FutureSize5']    = tab[i,34].tolist()
-         Tab['Tree2FutureSize6']    = tab[i,35].tolist()
-         Tab['Tree3FutureSize1']    = tab[i,36].tolist()
-         Tab['Tree3FutureSize2']    = tab[i,37].tolist()
-         Tab['Tree3FutureSize3']    = tab[i,38].tolist()
-         Tab['Tree3FutureSize4']    = tab[i,39].tolist()
-         Tab['Tree3FutureSize5']    = tab[i,40].tolist()
-         Tab['Tree3FutureSize6']    = tab[i,41].tolist()
-         Tab['Tree4FutureSize1']    = tab[i,42].tolist()
-         Tab['Tree4FutureSize2']    = tab[i,43].tolist()
-         Tab['Tree4FutureSize3']    = tab[i,44].tolist()
-         Tab['Tree4FutureSize4']    = tab[i,45].tolist()
-         Tab['Tree4FutureSize5']    = tab[i,46].tolist()
-         Tab['Tree4FutureSize6']    = tab[i,47].tolist()
-         values_list = [Tab]
-         ResultProxy = connection.execute(query,values_list)
+    with open(folder_usr + 'Task.mat', 'r') as file:
+        for line in file:
+            Tab = dict()
+            if not line.strip().startswith('#'):
+                parts = line.split()
+                if len(parts) == 48:
+                    Tab['TaskNo']              = parts[0]
+                    Tab['TrialNo']             = parts[1]
+                    Tab['BlockNo']             = parts[2]
+                    Tab['Horizon']             = parts[3]
+                    Tab['ItemNo']              = parts[4]
+                    Tab['InitialSampleNb']     = parts[5]
+                    Tab['UnusedTree']          = parts[6]      
+                    Tab['DisplayOrder1']       = parts[7]
+                    Tab['DisplayOrder2']       = parts[8]
+                    Tab['DisplayOrder3']       = parts[9]     
+                    Tab['TreePositions1']      = parts[10]
+                    Tab['TreePositions2']      = parts[11]
+                    Tab['TreePositions3']      = parts[12]
+                    Tab['TreePositions4']      = parts[13]       
+                    Tab['InitialSample1Tree']  = parts[14]
+                    Tab['InitialSample2Tree']  = parts[15]
+                    Tab['InitialSample3Tree']  = parts[16]
+                    Tab['InitialSample4Tree']  = parts[17]
+                    Tab['InitialSample5Tree']  = parts[18]
+                    Tab['InitialSample1Size']  = parts[19]
+                    Tab['InitialSample2Size']  = parts[20]
+                    Tab['InitialSample3Size']  = parts[21]
+                    Tab['InitialSample4Size']  = parts[22]
+                    Tab['InitialSample5Size']  = parts[23]
+                    Tab['Tree1FutureSize1']    = parts[24]
+                    Tab['Tree1FutureSize2']    = parts[25]
+                    Tab['Tree1FutureSize3']    = parts[26]
+                    Tab['Tree1FutureSize4']    = parts[27]
+                    Tab['Tree1FutureSize5']    = parts[28]
+                    Tab['Tree1FutureSize6']    = parts[29]
+                    Tab['Tree2FutureSize1']    = parts[30]
+                    Tab['Tree2FutureSize2']    = parts[31]
+                    Tab['Tree2FutureSize3']    = parts[32]
+                    Tab['Tree2FutureSize4']    = parts[33]
+                    Tab['Tree2FutureSize5']    = parts[34]
+                    Tab['Tree2FutureSize6']    = parts[35]
+                    Tab['Tree3FutureSize1']    = parts[36]
+                    Tab['Tree3FutureSize2']    = parts[37]
+                    Tab['Tree3FutureSize3']    = parts[38]
+                    Tab['Tree3FutureSize4']    = parts[39]
+                    Tab['Tree3FutureSize5']    = parts[40]
+                    Tab['Tree3FutureSize6']    = parts[41]
+                    Tab['Tree4FutureSize1']    = parts[42]
+                    Tab['Tree4FutureSize2']    = parts[43]
+                    Tab['Tree4FutureSize3']    = parts[44]
+                    Tab['Tree4FutureSize4']    = parts[45]
+                    Tab['Tree4FutureSize5']    = parts[46]
+                    Tab['Tree4FutureSize6']    = parts[47]
+            ResultProxy = connection.execute(query,[Tab])
    
     ###############################
     ###### Table: training  #######
@@ -99,22 +100,23 @@ for training_no in range(1,101):
     #Inserting many records at ones
     query  = db.insert(table) 
 
-    mat = scipy.io.loadmat(folder_usr + 'Training.mat')
-    tab = mat.get("Training")
-    for i in range(0,len(tab)):
-         Tab = dict()
-         Tab['TrainingNo']          = tab[i,0].tolist()
-         Tab['TrialNo']             = tab[i,1].tolist()
-         Tab['InitialSample1Size']  = tab[i,2].tolist()
-         Tab['InitialSample2Size']  = tab[i,3].tolist()
-         Tab['InitialSample3Size']  = tab[i,4].tolist()
-         Tab['Choice1Size']         = tab[i,5].tolist()
-         Tab['Choice2Size']         = tab[i,6].tolist()
-         Tab['Choice1Correct']      = tab[i,7].tolist()
-         Tab['Choice2Correct']      = tab[i,8].tolist()
-      
-         values_list = [Tab]
-         ResultProxy = connection.execute(query,values_list)
+    with open(folder_usr + 'Training.mat', 'r') as file:
+        for line in file:
+            Tab = dict()
+            if not line.strip().startswith('#'):
+                parts = line.split()
+                if len(parts) == 9:
+                    Tab['TrainingNo']          = parts[0]
+                    Tab['TrialNo']             = parts[1]
+                    Tab['InitialSample1Size']  = parts[2]
+                    Tab['InitialSample2Size']  = parts[3]
+                    Tab['InitialSample3Size']  = parts[4]
+                    Tab['Choice1Size']         = parts[5]
+                    Tab['Choice2Size']         = parts[6]
+                    Tab['Choice1Correct']      = parts[7]
+                    Tab['Choice2Correct']      = parts[8]
+            values_list = [Tab]
+            ResultProxy = connection.execute(query,values_list)
          
          
         
